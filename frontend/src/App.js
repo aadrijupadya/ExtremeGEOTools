3d8904973072761b41dcbd652fdd6c01b8cf296a
Aadrij Upadya <41775053+aadrijupadya@users.noreply.github.com>
2025-09-02 10:55:09 -0700

    more frontend routing debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import QueryPage from './components/QueryPage';
 import CostsPage from './components/CostsPage';
